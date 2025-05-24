0dc82f9c46acfbdf84f96c709400d89353fb14f9
Fireblade <72758695+Firebladedoge229@users.noreply.github.com>
2025-05-24 14:46:04 -0400

    remove GPL dependencies

--- src/main.py
+++ src/main.py
@@ -1,23 +1,10 @@
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication
-from qfluentwidgets import setTheme, Theme
 try:
     import ctypes
     kernel32 = ctypes.windll.kernel32
     kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
 except:
     pass
-from ui_components import Window
 
 if __name__ == "__main__":
-    QApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
-    QApplication.setAttribute(Qt.ApplicationAttribute.AA_DontCreateNativeWidgetSiblings)
-    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
-    setTheme(Theme.AUTO)
-
-    app = QApplication(sys.argv)
-    main_window = Window()
-    main_window.show()
-    app.exec_()
+    pass
